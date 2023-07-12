fe067526b7f0b7cfbebd5e1ce40fcb110fcfad8b
AtharvaMaskar <107984209+AtharvaMaskar26@users.noreply.github.com>
2023-07-12 23:48:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "navbar-red": "#F55A5A",
+      }
+    },
+  },
+  plugins: [],
+}
